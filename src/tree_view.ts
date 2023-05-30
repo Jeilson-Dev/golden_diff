9ea8bf7b8f98e566914da3b9a7dd2966d5a9ef43
Jeilson Araujo <jeilsonaraujo@icloud.com>
2023-05-30 12:16:30 -0300

    feat: change icon on primary sidebar, added other views to see individual teted master masked and isolated, addedd hover to easy toggle on tested and master image

--- src/tree_view.ts
+++ src/tree_view.ts
@@ -4,15 +4,19 @@ export namespace goldesNameSpace {
 
     class GoldenItem extends vscode.TreeItem {
         readonly label: string | undefined;
-        readonly imageMastered: string | undefined;
+        readonly imageMaster: string | undefined;
         readonly imageFailure: string | undefined;
+        readonly imageIsolated: string | undefined;
+        readonly imageMasked: string | undefined;
 
         public children: GoldenItem[] | undefined;
 
-        constructor(label: string, imageMastered: string, imageFailure: string) {
+        constructor(label: string, imageMaster: string, imageFailure: string, imageIsolated: string, imageMasked: string) {
             super(label, vscode.TreeItemCollapsibleState.None);
-            this.imageMastered = imageMastered;
+            this.imageMaster = imageMaster;
             this.imageFailure = imageFailure;
+            this.imageIsolated = imageIsolated;
+            this.imageMasked = imageMasked;
             this.collapsibleState = vscode.TreeItemCollapsibleState.None;
         }
 
@@ -63,8 +67,10 @@ export namespace goldesNameSpace {
                     let labelArray = failureImage.path.split('/');
                     let label = labelArray[labelArray.length - 1].replace('_testImage.png', '');
                     let imageFailure = failureImage.path;
-                    let imageMastered = failureImage.path.replace('_testImage.png', '_masterImage.png');
-                    return this.goldenData.push(new GoldenItem(label, imageMastered, imageFailure));
+                    let imageMaster = failureImage.path.replace('_testImage.png', '_masterImage.png');
+                    let imageIsolated = failureImage.path.replace('_testImage.png', '_isolatedDiff.png');
+                    let imageMasked = failureImage.path.replace('_testImage.png', '_maskedDiff.png');
+                    return this.goldenData.push(new GoldenItem(label, imageMaster, imageFailure, imageIsolated, imageMasked));
                 });
             }
         }
@@ -73,15 +79,43 @@ export namespace goldesNameSpace {
             panel.webview.html = `<!DOCTYPE html>
             <html lang="en">
             <head>
-                <meta charset="UTF-8">
+            <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Golden Diff</title>
-        
-                <style type="text/css">
+                <style>
+                    /* tabview style */
+                    .tab {
+                        overflow: hidden;
+                        background-color: #f1f1f1;
+                    }
+
+                    /* tbb style */
+                    .tab button {
+                        background-color: inherit;
+                        float: left;
+                        border: none;
+                        outline: none;
+                        cursor: pointer;
+                        padding: 14px 16px;
+                        transition: 0.3s;
+                    }
+
+                    /* tab selected background */
+                    .tab button.active {
+                        background-color: #ccc;
+                    }
+
+                    /* tab content style */
+                    .tabcontent {
+                        display: none;
+                        padding: 6px 12px;
+                        border: 1px solid #ccc;
+                        border-top: none;
+                    }
+
                     section div:first-child {
                         width: 1000px;
                         height: 600px;
-                        background: url('${panel.webview.asWebviewUri(vscode.Uri.file(item.imageMastered!))}');
+                        background: url('${panel.webview.asWebviewUri(vscode.Uri.file(item.imageMaster!))}');
                         background-repeat: no-repeat;
                         background-size: 1000px 600px;
                         position: relative;
@@ -106,23 +140,106 @@ export namespace goldesNameSpace {
                     }
                 </style>
             </head>
+
             <body>
-                <section>
-                    <div></div>
-                    <div id="last" style="width: 1000px;"></div>
-                </section>
-                <br>
-                <input id="slider" class="slider-width100" type="range" oninput="changeWidth(this.value)" min="0" max="100" value="100">
-            
+
+                <div class="tab">
+                    <button class="tablinks" onclick="openTab(event, 'Tab1')">Compare (Test - Master)</button>
+                    <button class="tablinks" onclick="openTab(event, 'Tab2')">Test Image</button>
+                    <button class="tablinks" onclick="openTab(event, 'Tab3')">Master Image</button>
+                    <button class="tablinks" onclick="openTab(event, 'Tab4')">Isolated Image</button>
+                    <button class="tablinks" onclick="openTab(event, 'Tab5')">Masked Image</button>
+                </div>
+
+                <div id="Tab1" class="tabcontent">
+                    <section>
+                        <div></div>
+                        <div id="last" style="width: 1000px;"></div>
+                    </section>
+                    <br>
+                    <input id="slider" class="slider-width100" type="range" oninput="changeWidth(this.value)" min="0" max="100" value="100">
+        
+                </div>
+
+                <div id="Tab2" class="tabcontent">
+                    <div style="background: url('${panel.webview.asWebviewUri(vscode.Uri.file(item.imageFailure!))}');  
+                    width: 1000px;
+                    height: 600px;
+                    background-repeat: no-repeat;
+                    background-size: 1000px 600px;" >
+                    </div>
+                    <div style="height: 37px;"></div>
+                    
+                </div>
+
+                <div id="Tab3" class="tabcontent">
+                <div style="background: url('${panel.webview.asWebviewUri(vscode.Uri.file(item.imageMaster!))}');  
+                width: 1000px;
+                height: 600px;
+                background-repeat: no-repeat;
+                background-size: 1000px 600px;" >
+                </div>
+                <div style="height: 37px;"></div>
+                </div>
+               
+                <div id="Tab4" class="tabcontent">
+                <div style="background: url('${panel.webview.asWebviewUri(vscode.Uri.file(item.imageIsolated!))}');  
+                width: 1000px;
+                height: 600px;
+                background-repeat: no-repeat;
+                background-size: 1000px 600px;" >
+                </div>
+                <div style="height: 37px;"></div>
+                </div>
+               
+                <div id="Tab5" class="tabcontent">
+                <div style="background: url('${panel.webview.asWebviewUri(vscode.Uri.file(item.imageMasked!))}');  
+                width: 1000px;
+                height: 600px;
+                background-repeat: no-repeat;
+                background-size: 1000px 600px;" >
+                </div>
+                <div style="height: 37px;"></div>
+                </div>
+
                 <script>
+                    function openTab(evt, tabName) {
+                        var i, tabcontent, tablinks;
+                        tabcontent = document.getElementsByClassName("tabcontent");
+                        for (i = 0; i < tabcontent.length; i++) {
+                            tabcontent[i].style.display = "none";
+                        }
+                        tablinks = document.getElementsByClassName("tablinks");
+                        for (i = 0; i < tablinks.length; i++) {
+                            tablinks[i].className = tablinks[i].className.replace(" active", "");
+                        }
+                        document.getElementById(tabName).style.display = "block";
+                        evt.currentTarget.className += " active";
+                    }
+
+                    document.getElementsByClassName("tablinks")[0].click();
+
                     const input = document.querySelector("#slider")
-                    input.addEventListener("input", (event) => {
-                        console.log(event.target.value);
-                        document.querySelector("#last").style.width = event.target.value * 10 + 'px';
-                    })
-                    function changeWidth(width) {}
+                    input.addEventListener("input", (event) => document.querySelector("#last").style.width = event.target.value * 10 + 'px');
+                    
+                    const element = document.querySelector("#last");
+
+                    element.addEventListener('mouseenter', () => {
+                        document.querySelector("#last").style.width='0px';
+                        document.querySelector("#slider").value=0;
+                      });
+                      
+                      element.addEventListener('mouseleave', () => {
+                        document.querySelector("#last").style.width='1000px';
+                        document.querySelector("#slider").value=100;
+
+
+                      });
+
                 </script>
+
             </body>
+    
             </html>`;
         }
     }
